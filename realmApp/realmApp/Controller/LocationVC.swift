c9445d54e15a97f33e183b3e7dd251153b48cb13
Stephenson Ang <stiph@Stephensons-MacBook-Pro.local>
2019-06-19 06:36:16 +0800

    setup mapView and shared LocationVC

---
+++ realmApp/realmApp/Controller/LocationVC.swift
@@ -0,0 +1,29 @@
+//
+//  LocationVC.swift
+//  realmApp
+//
+//  Created by Stephenson Ang on 19/06/2019.
+//  Copyright © 2019 Stephenson Ang. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocationVC: UIViewController, MKMapViewDelegate {
+    
+    var manager: CLLocationManager?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        manager = CLLocationManager()
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        manager?.activityType = .fitness
+    }
+    
+    func checkLocationAuthStatus() {
+        if CLLocationManager.authorizationStatus() != .authorizedWhenInUse {
+            manager?.requestWhenInUseAuthorization()
+        }
+    }
+    
+}
