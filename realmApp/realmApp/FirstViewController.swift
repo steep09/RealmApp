3741951be7ed1cf6f99b3e48e0977c859e13e6a4
Stephenson Ang <stiph@Stephensons-MacBook-Pro.local>
2019-06-19 04:56:02 +0800

    initial commit

---
+++ realmApp/realmApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  realmApp
+//
+//  Created by Stephenson Ang on 19/06/2019.
+//  Copyright © 2019 Stephenson Ang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ realmApp/realmApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  realmApp
+//
+//  Created by Stephenson Ang on 19/06/2019.
+//  Copyright © 2019 Stephenson Ang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
