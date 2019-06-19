d73a74cf15b3488a8043008a9ba56f81a6596a4c
Stephenson Ang <stiph@Stephensons-MacBook-Pro.local>
2019-06-19 12:38:11 +0800

    created the realm object

---
+++ realmApp/realmApp/Model/Run.swift
@@ -0,0 +1,35 @@
+//
+//  Run.swift
+//  realmApp
+//
+//  Created by Stephenson Ang on 19/06/2019.
+//  Copyright © 2019 Stephenson Ang. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Run: Object {
+    @objc dynamic public private(set) var id = ""
+    @objc dynamic public private(set) var date = NSDate()
+    @objc dynamic public private(set) var pace = 0
+    @objc dynamic public private(set) var distance = 0.0
+    @objc dynamic public private(set) var duration = 0
+    
+    override class func primaryKey() -> String {
+        return "id"
+    }
+    
+    override class func indexedProperties() -> [String] {
+        return ["date", "pace", "duration"]
+    }
+    
+    convenience init(pace: Int, distance: Double, duration: Int) {
+        self.init()
+        self.id = UUID().uuidString.lowercased()
+        self.date = NSDate()
+        self.pace = pace
+        self.distance = distance
+        self.duration = duration
+    }
+}
